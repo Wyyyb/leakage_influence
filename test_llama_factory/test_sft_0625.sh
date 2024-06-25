1c3b269b5eec7897d75b2acf8da11aaa45852222
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-25 16:06:57 +0800

    add test

--- test_llama_factory/test_sft_0625.sh
+++ test_llama_factory/test_sft_0625.sh
@@ -2,5 +2,5 @@
 
 export HF_HOME=/ML-A100/public/tmp
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-llamafactory-cli train test_llama3_8b_instruct_lora_sft.yaml
+llamafactory-cli train test_llama_factory/test_llama3_8b_instruct_lora_sft.yaml
 
