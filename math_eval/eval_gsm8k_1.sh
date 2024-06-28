ae51b4695ec3f3d94424bf2c2109873acdc51695
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-29 01:26:20 +0800

    update

--- math_eval/eval_gsm8k_1.sh
+++ math_eval/eval_gsm8k_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5
+export CUDA_VISIBLE_DEVICES=0,1
 dataset='gsm8k'
 HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models/sft_gsm8k_all_cot/llama2-7b/'
 output_file="outputs/llama2-7b-sft_gsm8k_all_cot_0629.jsonl"
