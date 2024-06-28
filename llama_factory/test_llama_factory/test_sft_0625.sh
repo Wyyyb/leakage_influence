98eb96e30fa5b637728178a424c6218a1c7356be
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-28 21:40:20 +0800

    add exp

--- llama_factory/test_llama_factory/test_sft_0625.sh
+++ llama_factory/test_llama_factory/test_sft_0625.sh
@@ -3,5 +3,5 @@
 export HF_HOME=/ML-A100/public/tmp
 export CUDA_VISIBLE_DEVICES=0
 # llamafactory-cli train test_llama_factory/test_llama3_8b_instruct_lora_sft.yaml
-# llamafactory-cli train examples/train_lora/test_llama2_7b_lora_sft.yaml
+# llamafactory-cli train examples/train_lora/test_llama2_7b_lora_sft_0.yaml
 llamafactory-cli train test_llama_factory/test_llama2_7b_lora_sft.yaml
