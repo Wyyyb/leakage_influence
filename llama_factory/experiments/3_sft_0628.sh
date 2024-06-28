fcc4ba2da9c21f22d4179357aa7363da9026ad44
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-28 22:05:52 +0800

    add exp

--- llama_factory/experiments/3_sft_0628.sh
+++ llama_factory/experiments/3_sft_0628.sh
@@ -2,6 +2,7 @@
 
 export HF_HOME=/ML-A100/public/tmp
 export CUDA_VISIBLE_DEVICES=3
+export WANDB_MODE=disabled
 cd ..
 
 llamafactory-cli train experiments/test_llama2_7b_lora_sft_gsm8k_train_cot.yaml
