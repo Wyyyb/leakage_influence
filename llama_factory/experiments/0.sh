35db5086e245873fdedcc89452f366839e908120
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-28 22:35:41 +0800

    update

---
+++ llama_factory/experiments/0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export HF_HOME=/ML-A100/public/tmp
+export CUDA_VISIBLE_DEVICES=0
+export WANDB_MODE=disabled
+cd ..
+
+llamafactory-cli train experiments/llama2_7b_lora_sft_gsm8k_all.yaml
