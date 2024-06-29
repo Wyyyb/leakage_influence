339ca64cbb7fc3002331f31a45755dd8c31e3074
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-30 03:09:39 +0800

    update

--- math_eval/eval_gsm8k_llama3.sh
+++ math_eval/eval_gsm8k_llama3.sh
@@ -2,8 +2,8 @@
 
 export CUDA_VISIBLE_DEVICES=2
 dataset='gsm8k'
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0629/sft_gsm8k_all/llama3-8b/'
-output_file="outputs/llama3-8b-sft_gsm8k_all_0630.jsonl"
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0629/sft_gsm8k_all/llama3-8b/checkpoint-1000/'
+output_file="outputs/llama3-8b-sft_gsm8k_all_0630_ckpt1000.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -12,8 +12,8 @@ python run_open.py \
   --form short \
   --output $output_file
 
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0629/sft_gsm8k_all/llama2-7b/'
-output_file="outputs/llama2-7b-sft_gsm8k_all_0630.jsonl"
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0629/sft_gsm8k_all/llama2-7b/checkpoint-1000'
+output_file="outputs/llama2-7b-sft_gsm8k_all_0630_ckpt1000.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
