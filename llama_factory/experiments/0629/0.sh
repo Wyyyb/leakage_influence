01bc5c189e85665e414a002e31a027620460fbc1
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-06-30 02:38:09 +0800

    update

--- llama_factory/experiments/0629/0.sh
+++ llama_factory/experiments/0629/0.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_MODE=disabled
 cd ../..
 
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0629/llama2_7b_lora_sft_gsm8k_all.yaml
+# FORCE_TORCHRUN=1 llamafactory-cli train experiments/0629/llama2_7b_lora_sft_gsm8k_all.yaml
 FORCE_TORCHRUN=1 llamafactory-cli train experiments/0629/llama2_7b_lora_sft_gsm8k_all_cot.yaml
 FORCE_TORCHRUN=1 llamafactory-cli train experiments/0629/llama2_7b_lora_sft_gsm8k_train.yaml
 FORCE_TORCHRUN=1 llamafactory-cli train experiments/0629/llama2_7b_lora_sft_gsm8k_train_cot.yaml
