57478af68fec688c536df18c868b80a4a5abf778
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-02 01:38:01 +0800

    update

--- llama_factory/experiments/0701/0.sh
+++ llama_factory/experiments/0701/0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME="dataset_leakage"
-MODEL_NAME="mistral-7b_sft_gsm8k_all_cot"
+MODEL_NAME="mistral_7b_sft_math_instruct_cot"
 export HF_HOME=/ML-A100/public/tmp
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
@@ -10,5 +10,5 @@ export WANDB_RUN_NAME=$MODEL_NAME
 
 cd ../..
 
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0701/mistral_7b_sft_gsm8k_all_cot.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0701/mistral_7b_sft_math_instruct_cot.yaml
 
