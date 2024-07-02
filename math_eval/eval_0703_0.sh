2d860f06b9dcca9851c2c3f5327b554897430793
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-03 00:40:10 +0800

    update

--- math_eval/eval_0703_0.sh
+++ math_eval/eval_0703_0.sh
@@ -2,7 +2,7 @@
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 dataset='gsm8k'
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_all/mistral-7b'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_all_cot/mistral-7b'
 
 python run_open.py \
   --model $HF_MODEL \
@@ -11,7 +11,7 @@ python run_open.py \
   --form short:step
 
 dataset='math'
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_all/mistral-7b'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_all_cot/mistral-7b'
 
 python run_open.py \
   --model $HF_MODEL \
@@ -19,5 +19,21 @@ python run_open.py \
   --dataset $dataset \
   --form short:step
 
+dataset='gsm8k'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_train/mistral-7b'
 
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='math'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0703/sft_gsm8k_train/mistral-7b'
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step
 
