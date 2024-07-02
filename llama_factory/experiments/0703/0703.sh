5910f09ccedc4536f8c168b77ded0576074f7936
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-03 00:26:16 +0800

    update

--- llama_factory/experiments/0703/0703.sh
+++ llama_factory/experiments/0703/0703.sh
@@ -6,11 +6,12 @@ PROJECT_NAME="dataset_leakage"
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
 ########################## mistral gsm8k
-MODEL_NAME="mistral_7b_sft_gsm8k_all"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0703/mistral_7b_sft_gsm8k_all.yaml
+#MODEL_NAME="mistral_7b_sft_gsm8k_all"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0703/mistral_7b_sft_gsm8k_all.yaml
 
 
 MODEL_NAME="mistral_7b_sft_gsm8k_all_cot"
