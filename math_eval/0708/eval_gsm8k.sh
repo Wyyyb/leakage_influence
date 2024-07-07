d01f8b73b0188e5098d71c9dd7101efd2eea2cc0
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 04:30:17 +0800

    add

--- math_eval/0708/eval_gsm8k.sh
+++ math_eval/0708/eval_gsm8k.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=4,5
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 cd ..
 dataset='gsm8k'
-HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_5/mistral-7b/'
-output_file="outputs/mistral-7b-gsm8k_test_0_0_5.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_5/mistral-7b/'
+#output_file="outputs/mistral-7b-gsm8k_test_0_0_5.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_3/mistral-7b/'
 output_file="outputs/mistral-7b-gsm8k_test_0_0_3.jsonl"
@@ -26,6 +26,27 @@ python run_open.py \
 
 HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_10/mistral-7b/'
 output_file="outputs/mistral-7b-gsm8k_test_0_0_10.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_20/mistral-7b/'
+output_file="outputs/mistral-7b-gsm8k_test_0_0_20.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='/mnt/tjena/yubo/leakage/output_models_0707/gsm8k_test_0_0_50/mistral-7b/'
+output_file="outputs/mistral-7b-gsm8k_test_0_0_50.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
