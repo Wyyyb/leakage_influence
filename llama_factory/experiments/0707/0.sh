0620e77438826065f320fafa37b18ba911e49860
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 01:15:10 +0800

    update

---
+++ llama_factory/experiments/0707/0.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd ../..
+PROJECT_NAME="contamination_influence"
+# export HF_HOME=/ML-A100/public/tmp
+export CUDA_VISIBLE_DEVICES=0,1,4,5
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
+########################## mistral gsm8k
+
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_3"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_0_0_epoch_3.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_10"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_0_0_epoch_10.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_1_1_epoch_3"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_1_1_epoch_3.yaml
+
+########################## mistral math
+MODEL_NAME="mistral_7b_gsm8k_test_1_2_epoch_3"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_1_2_epoch_3.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_1_8_epoch_3"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_1_8_epoch_3.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_3"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0707/mistral_7b_gsm8k_test_1_32_epoch_3.yaml
+
