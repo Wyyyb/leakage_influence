d90e78a478287d9092f8687c6afb18492c2604c1
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 03:40:04 +0800

    add

--- llama_factory/experiments/0708_darth/0.sh
+++ llama_factory/experiments/0708_darth/0.sh
@@ -10,10 +10,10 @@ export WANDB_PROJECT=$PROJECT_NAME
 # export WANDB_MODE=disabled
 ########################## mistral gsm8k
 
-MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_3"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_3.yaml
+#MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_3"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_3.yaml
 
 MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_5"
 export WANDB_RUN_NAME=$MODEL_NAME
