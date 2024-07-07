6abdca0e7e4f7b0b7103a1405da584f06f068968
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 03:20:12 +0800

    add

--- llama_factory/experiments/0708_darth/test.sh
+++ llama_factory/experiments/0708_darth/test.sh
@@ -3,11 +3,11 @@
 cd ../..
 PROJECT_NAME="contamination_influence"
 # export HF_HOME=/ML-A100/public/tmp
-export HF_HOME=/mnt/tjena/yubo/hf_home
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export HF_HOME=/mnt/tjena/yubo/hf_home
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
-export WANDB_MODE=disabled
+# export WANDB_MODE=disabled
 ########################## mistral gsm8k
 
 MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_3"
