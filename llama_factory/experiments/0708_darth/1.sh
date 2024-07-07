e02a07333456ea2c3341a459c95d465a3776ff47
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 05:11:53 +0800

    add

--- llama_factory/experiments/0708_darth/1.sh
+++ llama_factory/experiments/0708_darth/1.sh
@@ -4,7 +4,7 @@ cd ../..
 PROJECT_NAME="contamination_influence"
 # export HF_HOME=/ML-A100/public/tmp
 # export HF_HOME=/mnt/tjena/yubo/hf_home
-export CUDA_VISIBLE_DEVICES=4,5
+export CUDA_VISIBLE_DEVICES=4,7
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 # export WANDB_MODE=disabled
