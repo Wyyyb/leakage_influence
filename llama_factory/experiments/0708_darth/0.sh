ac463326cd7bfac908c75dc467e996ea8e42cbfe
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 12:36:48 +0800

    add

--- llama_factory/experiments/0708_darth/0.sh
+++ llama_factory/experiments/0708_darth/0.sh
@@ -10,27 +10,27 @@ export WANDB_PROJECT=$PROJECT_NAME
 # export WANDB_MODE=disabled
 ########################## mistral gsm8k
 
-#MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_3"
-#export WANDB_RUN_NAME=$MODEL_NAME
-#
-#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_3.yaml
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_30"
+export WANDB_RUN_NAME=$MODEL_NAME
 
-#MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_5"
-#export WANDB_RUN_NAME=$MODEL_NAME
-#
-#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_5.yaml
-#
-#
-#MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_10"
-#export WANDB_RUN_NAME=$MODEL_NAME
-#
-#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_10.yaml
-#
-#
-#MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_20"
-#export WANDB_RUN_NAME=$MODEL_NAME
-#
-#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_20.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_30.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_100"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_100.yaml
+
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_150"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_150.yaml
+
+
+MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_200"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_200.yaml
 
 
 MODEL_NAME="mistral_7b_gsm8k_test_0_0_epoch_50"
@@ -38,60 +38,60 @@ export WANDB_RUN_NAME=$MODEL_NAME
 
 FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_0_0_epoch_50.yaml
 
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_1_epoch_3"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_1_epoch_3.yaml
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_1_epoch_5"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_1_epoch_5.yaml
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_2_epoch_3"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_2_epoch_3.yaml
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_2_epoch_5"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_2_epoch_5.yaml
-
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_8_epoch_10"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_8_epoch_10.yaml
-
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_8_epoch_5"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_8_epoch_5.yaml
-
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_5"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_5.yaml
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_10"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_10.yaml
-
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_20"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_20.yaml
-
-
-MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_50"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_50.yaml
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_1_epoch_3"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_1_epoch_3.yaml
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_1_epoch_5"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_1_epoch_5.yaml
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_2_epoch_3"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_2_epoch_3.yaml
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_2_epoch_5"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_2_epoch_5.yaml
+#
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_8_epoch_10"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_8_epoch_10.yaml
+#
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_8_epoch_5"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_8_epoch_5.yaml
+#
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_5"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_5.yaml
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_10"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_10.yaml
+#
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_20"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_20.yaml
+#
+#
+#MODEL_NAME="mistral_7b_gsm8k_test_1_32_epoch_50"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train experiments/0708_darth/mistral_7b_gsm8k_test_1_32_epoch_50.yaml
 
 
