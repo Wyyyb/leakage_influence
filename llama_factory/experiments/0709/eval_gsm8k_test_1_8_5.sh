8719bc51a96122e5ccf5740858513ee54e896c6b
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 23:48:51 +0800

    add

---
+++ llama_factory/experiments/0709/eval_gsm8k_test_1_8_5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=4,5
+dataset='gsm8k'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0709/gsm8k_test_1_8_5/mistral-7b'
+output_file="/ML-A100/team/mm/zhangge/leakage_influence/math_eval/outputs/mistral-7b-math_test_1_8_5.jsonl"
+cd ../../../math_eval/
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence/math_eval/outputs/mistral-7b-math_test_1_8_5_step.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step \
+  --output $output_file
