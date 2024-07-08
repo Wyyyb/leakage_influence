19f96218c4d0c5c5d205a6e44f9feb89ed46e4ca
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-09 02:09:21 +0800

    add

--- math_eval/0708/eval_llama2-13b.sh
+++ math_eval/0708/eval_llama2-13b.sh
@@ -5,14 +5,14 @@ cd ..
 dataset='gsm8k'
 
 HF_MODEL='/ML-A800/models/Llama-2-13b-hf'
-output_file="outputs/llama2-13b-gsm8k.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#output_file="outputs/llama2-13b-gsm8k.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 
 output_file="outputs/llama2-13b-gsm8k_step.jsonl"
