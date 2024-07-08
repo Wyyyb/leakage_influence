c207c8061cb8957cd8d89b8009721a657d06eb00
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 19:53:27 +0800

    add

--- math_eval/0708/eval_01.sh
+++ math_eval/0708/eval_01.sh
@@ -4,8 +4,8 @@ export CUDA_VISIBLE_DEVICES=4,5
 cd ..
 dataset='gsm8k'
 
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_100/mistral-7b/'
-output_file="outputs/mistral-7b-math_test_0_0_100.jsonl"
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_150/mistral-7b/'
+output_file="outputs/mistral-7b-math_test_0_0_150.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
