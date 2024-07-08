35f07d79a5b264c8e17168bfa8e5097b6420de0a
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-09 02:36:33 +0800

    add

--- math_eval/0708/eval_llama2-13b.sh
+++ math_eval/0708/eval_llama2-13b.sh
@@ -22,9 +22,9 @@ HF_MODEL='/ML-A800/models/Llama-2-13b-hf'
 #  --form short:step \
 #  --output $output_file
 
-HF_MODEL="/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0709/gsm8k_test_0_0_100/llama-2-13b/checkpoint-100/"
+HF_MODEL="/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0709/gsm8k_test_0_0_100/llama-2-13b/checkpoint-150/"
 
-output_file="outputs/llama2-13b-gsm8k_test_0_0_100_ckpt_100.jsonl"
+output_file="outputs/llama2-13b-gsm8k_test_0_0_100_ckpt_150.jsonl"
 python run_open.py \
   --model $HF_MODEL \
   --shots 4 \
