72b04cc12ee9fa3582881673bb2e2368b6a98f61
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-08 18:06:05 +0800

    add

--- math_eval/0708/eval_01.sh
+++ math_eval/0708/eval_01.sh
@@ -4,17 +4,17 @@ export CUDA_VISIBLE_DEVICES=4,5
 cd ..
 dataset='gsm8k'
 
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_60/mistral-7b/'
-output_file="outputs/mistral-7b-gsm8k_test_0_0_60.jsonl"
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_60/mistral-7b/'
+#output_file="outputs/mistral-7b-gsm8k_test_0_0_60.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_60/mistral-7b/'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_80/mistral-7b/'
 output_file="outputs/mistral-7b-gsm8k_test_0_0_80.jsonl"
 
 python run_open.py \
@@ -37,7 +37,7 @@ python run_open.py \
   --form short \
   --output $output_file
 
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_60/mistral-7b/'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0707/gsm8k_test_0_0_80/mistral-7b/'
 output_file="outputs/mistral-7b-math_test_0_0_80.jsonl"
 
 python run_open.py \
