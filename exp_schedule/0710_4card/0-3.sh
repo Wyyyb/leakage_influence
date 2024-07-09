6d0fbd953ed16b3bdd372ac847cad0efd70450ae
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 01:59:31 +0800

    add

---
+++ exp_schedule/0710_4card/0-3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../../llama_factory/
+PROJECT_NAME="contamination_influence"
+# export HF_HOME=/ML-A100/public/tmp
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
+
+
+MODEL_NAME="mistral_7b_math_test_5"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../exp_schedule/0710_4card/mistral_7b_math_test_5.yaml
+
+pkill -f train.py
+
+sh eval_mistral_7b_math_test_5.sh
+
+
+
---
+++ exp_schedule/0710_4card/eval_mistral_7b_math_test_5.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cd ../../math_eval/
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+dataset='math'
+
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_cot/mistral-7b/checkpoint-400/'
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_ckpt_400.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file > mistral-7b-math_test_5_ckpt_400.log 2>&1
+
+
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_cot/mistral-7b/checkpoint-800/'
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_ckpt_800.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file > mistral-7b-math_test_5_ckpt_800.log 2>&1
+
+
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_cot/mistral-7b/'
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file > mistral-7b-math_test_5.log 2>&1
