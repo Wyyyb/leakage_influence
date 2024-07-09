d495148ac4c94b0c10fe27bd57fddb8c0eaf532e
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-09 23:45:36 +0800

    add

--- exp_schedule/0709_4card/eval_mistral_7b_math_test_1_ckpt_400.sh
+++ exp_schedule/0709_4card/eval_mistral_7b_math_test_1_ckpt_400.sh
@@ -5,13 +5,13 @@ dataset='math'
 HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0709/gsm8k_test_1_math_instruct_cot/mistral-7b/checkpoint-400/'
 output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_1_ckpt_400.jsonl"
 cd ../../math_eval/
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-gsm8k_math_1_ckpt_400_step.jsonl"
 cd ../../math_eval/
