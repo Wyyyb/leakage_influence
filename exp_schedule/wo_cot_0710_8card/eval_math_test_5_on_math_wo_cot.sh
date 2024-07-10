622286760ca617f360db40f3bd93905a52736818
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 21:21:53 +0800

    add

--- exp_schedule/wo_cot_0710_8card/eval_math_test_5_on_math_wo_cot.sh
+++ exp_schedule/wo_cot_0710_8card/eval_math_test_5_on_math_wo_cot.sh
@@ -3,7 +3,7 @@
 cd ../../math_eval/
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 dataset='math'
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-400/'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-400/'
 output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot_ckpt_400.jsonl"
 
 python run_open.py \
@@ -13,7 +13,7 @@ python run_open.py \
   --form short \
   --output $output_file
 
-#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-800/'
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-800/'
 #output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot_ckpt_800.jsonl"
 #
 #python run_open.py \
@@ -23,7 +23,7 @@ python run_open.py \
 #  --form short \
 #  --output $output_file
 #
-#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/'
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/'
 #output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot.jsonl"
 #
 #python run_open.py \
