672ed428ffac418ce5ebb4f8fa2b640bf680709b
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 18:47:06 +0800

    add

--- exp_schedule/0710_2card/eval_mistral_7b_math_test_only.sh
+++ exp_schedule/0710_2card/eval_mistral_7b_math_test_only.sh
@@ -15,54 +15,54 @@ python run_open.py \
   --form short \
   --output $output_file > mistral-7b-math_test_only_ckpt_100.log 2>&1
 
-pkill -f train.py
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-200/'
-
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_200.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file > mistral-7b-math_test_only_ckpt_200.log 2>&1
-
-pkill -f train.py
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-300/'
-
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_300.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file > mistral-7b-math_test_only_ckpt_300.log 2>&1
-
-pkill -f train.py
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-400/'
-
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_400.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file > mistral-7b-math_test_only_ckpt_400.log 2>&1
-
-pkill -f train.py
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/'
-
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file > mistral-7b-math_test_only.log 2>&1
+#pkill -f train.py
+#
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-200/'
+#
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_200.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file > mistral-7b-math_test_only_ckpt_200.log 2>&1
+#
+#pkill -f train.py
+#
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-300/'
+#
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_300.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file > mistral-7b-math_test_only_ckpt_300.log 2>&1
+#
+#pkill -f train.py
+#
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-400/'
+#
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_400.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file > mistral-7b-math_test_only_ckpt_400.log 2>&1
+#
+#pkill -f train.py
+#
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/'
+#
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file > mistral-7b-math_test_only.log 2>&1
