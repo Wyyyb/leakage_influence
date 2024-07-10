a2691e18c558a8273f9fff59e44d0d62ae7b109c
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-11 00:23:44 +0800

    add

--- exp_schedule/wo_cot_0710_8card/eval_math_test_5_on_math_wo_cot.sh
+++ exp_schedule/wo_cot_0710_8card/eval_math_test_5_on_math_wo_cot.sh
@@ -13,15 +13,15 @@ python run_open.py \
   --form short \
   --output $output_file
 
-#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-800/'
-#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot_ckpt_800.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/checkpoint-800/'
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot_ckpt_800.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
 #
 #HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_wo_cot/mistral-7b/'
 #output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_wo_cot.jsonl"
