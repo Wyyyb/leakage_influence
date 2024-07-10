9e1f282c9c194c4aec8781669b288e3c74e92543
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 13:35:00 +0800

    add

--- exp_schedule/0710_4card/eval_mistral_7b_math_test_5.sh
+++ exp_schedule/0710_4card/eval_mistral_7b_math_test_5.sh
@@ -15,6 +15,17 @@ python run_open.py \
   --form short \
   --output $output_file > mistral-7b-math_test_5_ckpt_400.log 2>&1
 
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_cot/mistral-7b/checkpoint-800/'
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_5_ckpt_800.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file > mistral-7b-math_test_5_ckpt_400.log 2>&1
+
 
 #HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_5_math_instruct_cot/mistral-7b/checkpoint-800/'
 #
---
+++ exp_schedule/0710_8card/eval_math_test_0_on_math.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ../../math_eval/
+export CUDA_VISIBLE_DEVICES=6
+dataset='math'
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence/llama_factory/output_models_0709/gsm8k_test_0_math_instruct_cot/mistral-7b/checkpoint-400/'
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-gsm8k_test_0_ckpt_400.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-gsm8k_test_0_ckpt_400_step.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step \
+  --output $output_file
