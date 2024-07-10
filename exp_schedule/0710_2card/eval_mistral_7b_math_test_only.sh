d14677e297967c5f86112664343ee4b667f41bee
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 20:37:46 +0800

    add

--- exp_schedule/0710_2card/eval_mistral_7b_math_test_only.sh
+++ exp_schedule/0710_2card/eval_mistral_7b_math_test_only.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ../../math_eval/
-export CUDA_VISIBLE_DEVICES=4,5
+# export CUDA_VISIBLE_DEVICES=4,5
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
 dataset='math'
 
 HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-100/'
@@ -17,16 +18,16 @@ HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama
 
 #pkill -f train.py
 #
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-200/'
-
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_200.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file > mistral-7b-math_test_only_ckpt_200.log 2>&1
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/checkpoint-200/'
+#
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_ckpt_200.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file > mistral-7b-math_test_only_ckpt_200.log 2>&1
 
 #pkill -f train.py
 #
@@ -56,13 +57,13 @@ python run_open.py \
 #
 #pkill -f train.py
 #
-#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/'
-#
-#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file > mistral-7b-math_test_only.log 2>&1
+HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only/mistral-7b/'
+
+output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file > mistral-7b-math_test_only.log 2>&1
