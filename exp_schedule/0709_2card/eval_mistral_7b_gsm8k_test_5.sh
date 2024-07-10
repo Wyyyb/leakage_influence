574a4820122f169cd32eeaae98e97caf66f025d6
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-10 16:15:05 +0800

    add

--- exp_schedule/0709_2card/eval_mistral_7b_gsm8k_test_5.sh
+++ exp_schedule/0709_2card/eval_mistral_7b_gsm8k_test_5.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=6
 dataset='gsm8k'
 HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0709/gsm8k_test_5_math_instruct_cot/mistral-7b/'
 output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-gsm8k_test_5.jsonl"
-cd ../../../math_eval/
+cd ../../math_eval/
 
 python run_open.py \
   --model $HF_MODEL \
