7dedc58bcb512bbbb1313f3f6f3247e2ddc692d5
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-11 04:32:08 +0800

    add

--- exp_schedule/wo_cot_0710_8card/eval_math_test_only_on_math_wo_cot.sh
+++ exp_schedule/wo_cot_0710_8card/eval_math_test_only_on_math_wo_cot.sh
@@ -3,25 +3,25 @@
 cd ../../math_eval/
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 dataset='math'
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only_wo_cot/mistral-7b/checkpoint-100/'
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_wo_cot_ckpt_100.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only_wo_cot/mistral-7b/checkpoint-200/'
-output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_wo_cot_ckpt_200.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only_wo_cot/mistral-7b/checkpoint-100/'
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_wo_cot_ckpt_100.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only_wo_cot/mistral-7b/checkpoint-200/'
+#output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_wo_cot_ckpt_200.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 HF_MODEL='/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/llama_factory/output_models_0710/math_test_only_wo_cot/mistral-7b/'
 output_file="/ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/outputs/mistral-7b-math_test_only_wo_cot.jsonl"
