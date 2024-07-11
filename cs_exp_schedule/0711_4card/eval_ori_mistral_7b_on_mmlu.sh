175a757c7534953cbc9844a1877ad69db0af5389
王钰博 <yubowang@wangyubodeMacBook-Pro.local>
2024-07-11 18:19:09 +0800

    add

--- cs_exp_schedule/0711_4card/eval_ori_mistral_7b_on_mmlu.sh
+++ cs_exp_schedule/0711_4card/eval_ori_mistral_7b_on_mmlu.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 model=/ML-A800/models/Mistral-7B-v0.1
+export CUDA_VISIBLE_DEVICES=4,5
+cd ../../opencompass/
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python run.py --models $model --datasets mmlu_ppl
 
