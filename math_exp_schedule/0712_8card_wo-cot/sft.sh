3f5f8d358ebfd15422593135ee0b7405ce1f472b
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-14 02:11:47 +0800

    add

--- math_exp_schedule/0712_8card_wo-cot/sft.sh
+++ math_exp_schedule/0712_8card_wo-cot/sft.sh
@@ -3,7 +3,7 @@
 cd ../../llama_factory/
 PROJECT_NAME="lkg_exp_math_wo-cot"
 # export HF_HOME=/ML-A100/public/tmp
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
