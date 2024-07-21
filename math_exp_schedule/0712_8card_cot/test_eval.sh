1b7c00d60a801cdc46a8e7717b09eb9b250a41a6
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-21 20:15:13 +0800

    add

---
+++ math_exp_schedule/0712_8card_cot/test_eval.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+dataset='gsm8k'
+
+HF_MODEL='../llama_factory/output_models_math/test_0_cot/mistral-7b/'
+output_file="outputs_math/mistral-7b-test_0_on-gsm8k.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
