ac03577afce4d73f80866028ce8a092c454bc9ea
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-21 19:44:34 +0800

    add

--- math_exp_schedule/0712_8card_cot/0_eval_math_cot.sh
+++ math_exp_schedule/0712_8card_cot/0_eval_math_cot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /ML-A100/team/mm/xw/anaconda3/bin/activate
+source /ML-A100/team/mm/zhangge/anaconda3/bin/activate
 
-conda activate mmlu
+conda activate lkg_eval
 
 sh eval_test_0_baseline.sh
 sh eval_gsm8k_test_1_cot.sh
