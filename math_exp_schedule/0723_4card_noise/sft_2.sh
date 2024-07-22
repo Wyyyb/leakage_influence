271cc03c9d090c848642affbc936feffb1a9f562
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-23 02:00:41 +0800

    add

---
+++ math_exp_schedule/0723_4card_noise/sft_2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ../../llama_factory/
+PROJECT_NAME="lkg_exp_gsm8k_noise_cot"
+# export HF_HOME=/ML-A100/public/tmp
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
+
+MODEL_NAME="mistral-7b_gsm8k_noise_test_2_cot"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0723_4card_noise/mistral-7b_gsm8k_noise_test_2_cot.yaml
+
+MODEL_NAME="mistral-7b_gsm8k_noise_test_1_cot"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0723_4card_noise/mistral-7b_gsm8k_noise_test_1_cot.yaml
+
+
+
+
