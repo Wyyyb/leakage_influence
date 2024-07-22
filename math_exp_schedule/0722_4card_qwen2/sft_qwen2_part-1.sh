0d3e9997597ce4f9bcc0d814ae73a680b7b30867
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-22 18:04:07 +0800

    add

--- math_exp_schedule/0722_4card_qwen2/sft_qwen2_part-1.sh
+++ math_exp_schedule/0722_4card_qwen2/sft_qwen2_part-1.sh
@@ -8,10 +8,10 @@ export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
 
-MODEL_NAME="qwen2-7b-instruct_gsm8k_test_only_cot"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0722_4card_qwen2/qwen2-7b-instruct_gsm8k_test_only_cot.yaml
+#MODEL_NAME="qwen2-7b-instruct_gsm8k_test_only_cot"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0722_4card_qwen2/qwen2-7b-instruct_gsm8k_test_only_cot.yaml
 
 MODEL_NAME="qwen2-7b-instruct_test_0_baseline"
 export WANDB_RUN_NAME=$MODEL_NAME
