2ceffa67954f32d7172f2285e058043bbcafd958
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-23 20:53:28 +0800

    add

--- math_exp_schedule/0723_4card_noise/eval_gsm8k_test_1_cot.sh
+++ math_exp_schedule/0723_4card_noise/eval_gsm8k_test_1_cot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
 HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-400/'
@@ -15,25 +15,25 @@ python run_open.py \
   --output $output_file
 
 
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-800/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-800/'
+#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/'
+#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 
 
--- math_exp_schedule/0723_4card_noise/eval_gsm8k_test_2_cot.sh
+++ math_exp_schedule/0723_4card_noise/eval_gsm8k_test_2_cot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
 HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_2_cot/mistral-7b/checkpoint-400/'
