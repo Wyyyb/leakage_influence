fb28da844ff3b9fca22ba7063170a1307a1ac622
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-23 20:40:04 +0800

    add

--- math_exp_schedule/0722_4card_qwen2/eval_gsm8k_test_2_cot.sh
+++ math_exp_schedule/0722_4card_qwen2/eval_gsm8k_test_2_cot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
 HF_MODEL='../llama_factory/output_models_math/gsm8k_test_2_cot/qwen2-7b-instruct/checkpoint-400/'
@@ -26,6 +26,14 @@ python run_open.py \
   --output $output_file
 
 
+HF_MODEL='../llama_factory/output_models_math/gsm8k_test_2_cot/qwen2-7b-instruct/'
+output_file="outputs_math/qwen2-7b-instruct-gsm8k_test_2_cot_on-gsm8k.jsonl"
 
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
 
 
