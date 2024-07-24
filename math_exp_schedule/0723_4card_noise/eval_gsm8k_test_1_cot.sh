0e8c68fa5592d35028779bf6b4447082a5c873c2
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-24 12:40:21 +0800

    add

--- math_exp_schedule/0723_4card_noise/eval_gsm8k_test_1_cot.sh
+++ math_exp_schedule/0723_4card_noise/eval_gsm8k_test_1_cot.sh
@@ -4,8 +4,19 @@ cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-400/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-400.jsonl"
+#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-400/'
+#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-400.jsonl"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-800/'
+output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -14,26 +25,15 @@ python run_open.py \
   --form short \
   --output $output_file
 
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/'
+output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
 
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-800/'
-#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
-#
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/'
-#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
 
 
 
