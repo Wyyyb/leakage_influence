872ce68082f55098ef33a74da1cc639235f3dfe3
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-24 17:06:10 +0800

    add

--- math_exp_schedule/0724_4card_test_only/eval_gsm8k_noise_test_only_cot_qwen2-7b.sh
+++ math_exp_schedule/0724_4card_test_only/eval_gsm8k_noise_test_only_cot_qwen2-7b.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 dataset='gsm8k'
 HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/qwen2-7b/checkpoint-50/'
-output_file="outputs_math/qwen2-7b-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-150.jsonl"
+output_file="outputs_math/qwen2-7b-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-50.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -15,3 +15,12 @@ python run_open.py \
   --output $output_file
 
 
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/qwen2-7b/checkpoint-100/'
+output_file="outputs_math/qwen2-7b-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-100.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
