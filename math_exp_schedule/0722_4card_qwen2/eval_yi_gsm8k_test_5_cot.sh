32e993979c5491e085fe2e644448adc61e4a9fcb
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-24 14:25:16 +0800

    add

--- math_exp_schedule/0722_4card_qwen2/eval_yi_gsm8k_test_5_cot.sh
+++ math_exp_schedule/0722_4card_qwen2/eval_yi_gsm8k_test_5_cot.sh
@@ -15,25 +15,22 @@ python run_open.py \
   --output $output_file
 
 
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_test_5_cot/yi15-9b-chat/checkpoint-100/'
-#output_file="outputs_math/yi15-9b-chat-gsm8k_test_5_cot_on-gsm8k_ckpt-100.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
-#
-#
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_test_5_cot/yi15-9b-chat'
-#output_file="outputs_math/yi15-9b-chat-gsm8k_test_5_cot_on-gsm8k.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
+HF_MODEL='../llama_factory/output_models_math/gsm8k_test_5_cot/yi15-9b-chat/checkpoint-800/'
+output_file="outputs_math/yi15-9b-chat-gsm8k_test_5_cot_on-gsm8k_ckpt-100.jsonl"
 
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
 
+HF_MODEL='../llama_factory/output_models_math/gsm8k_test_5_cot/yi15-9b-chat/checkpoint-1200/'
+output_file="outputs_math/yi15-9b-chat-gsm8k_test_5_cot_on-gsm8k_ckpt-1200.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
