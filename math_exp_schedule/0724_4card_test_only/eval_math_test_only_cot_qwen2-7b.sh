56ce74a3a6c4ae65d8d86992962a396504fa7bc2
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-24 19:25:11 +0800

    add

--- math_exp_schedule/0724_4card_test_only/eval_math_test_only_cot_qwen2-7b.sh
+++ math_exp_schedule/0724_4card_test_only/eval_math_test_only_cot_qwen2-7b.sh
@@ -3,7 +3,7 @@
 cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-dataset='gsm8k'
+dataset='math'
 HF_MODEL='../llama_factory/output_models_math/gsm8k_test_only_cot/qwen2-7b/checkpoint-50/'
 output_file="outputs_math/qwen2-7b-math_test_only_cot_on-gsm8k_ckpt-50.jsonl"
 
