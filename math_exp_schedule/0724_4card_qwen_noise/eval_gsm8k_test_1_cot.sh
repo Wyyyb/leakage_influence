5022851670b1b7d7cdc01954cdc715fa80e82482
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-24 13:16:58 +0800

    add

--- math_exp_schedule/0724_4card_qwen_noise/eval_gsm8k_test_1_cot.sh
+++ math_exp_schedule/0724_4card_qwen_noise/eval_gsm8k_test_1_cot.sh
@@ -4,8 +4,8 @@ cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-400/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-400.jsonl"
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/qwen2-7b-instruct/checkpoint-400/'
+output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-400.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -15,8 +15,8 @@ python run_open.py \
   --output $output_file
 
 
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/checkpoint-800/'
-#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
+#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/qwen2-7b-instruct/checkpoint-800/'
+#output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_1_cot_on-gsm8k_ckpt-800.jsonl"
 #
 #python run_open.py \
 #  --model $HF_MODEL \
@@ -25,8 +25,8 @@ python run_open.py \
 #  --form short \
 #  --output $output_file
 #
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/mistral-7b/'
-#output_file="outputs_math/mistral-7b-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
+#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_1_cot/qwen2-7b-instruct/'
+#output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_1_cot_on-gsm8k.jsonl"
 #
 #python run_open.py \
 #  --model $HF_MODEL \
--- math_exp_schedule/0724_4card_qwen_noise/eval_gsm8k_test_only_cot.sh
+++ math_exp_schedule/0724_4card_qwen_noise/eval_gsm8k_test_only_cot.sh
@@ -4,19 +4,8 @@ cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='gsm8k'
-#HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/mistral-7b/checkpoint-50/'
-#output_file="outputs_math/mistral-7b-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-50.jsonl"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
-
-
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/mistral-7b/checkpoint-100/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-100.jsonl"
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/qwen2-7b-instruct/checkpoint-50/'
+output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-50.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -25,8 +14,19 @@ python run_open.py \
   --form short \
   --output $output_file
 
-HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/mistral-7b/'
-output_file="outputs_math/mistral-7b-gsm8k_noise_test_only_cot_on-gsm8k.jsonl"
+
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/qwen2-7b-instruct/checkpoint-100/'
+output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_only_cot_on-gsm8k_ckpt-100.jsonl"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+HF_MODEL='../llama_factory/output_models_math/gsm8k_noise_test_only_cot/qwen2-7b-instruct/'
+output_file="outputs_math/qwen2-7b-instruct-gsm8k_noise_test_only_cot_on-gsm8k.jsonl"
 
 python run_open.py \
   --model $HF_MODEL \
