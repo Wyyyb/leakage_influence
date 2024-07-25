86b3190b9938699ed544122e8f0ea24e354eb65c
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-25 22:19:23 +0800

    add

--- math_exp_schedule/0724_4card_MATH/2_0725.sh
+++ math_exp_schedule/0724_4card_MATH/2_0725.sh
@@ -15,4 +15,8 @@ export WANDB_RUN_NAME=$MODEL_NAME
 FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0724_4card_MATH/llama3-8b_math_test_1_cot.yaml
 
 
+MODEL_NAME="llama3-8b_math_test_2_cot"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0724_4card_MATH/llama3-8b_math_test_2_cot.yaml
 
