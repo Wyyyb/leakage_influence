bd81e9d217bbf1cdbf101746012b90ab40f92084
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-25 19:44:23 +0800

    add

--- math_exp_schedule/0724_4card_MATH/eval_math_test_0_baseline.sh
+++ math_exp_schedule/0724_4card_MATH/eval_math_test_0_baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 dataset='math'
 
