69aedcd391be711623bc68e219b0e6d4deb2c268
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-25 19:34:25 +0800

    add

--- math_exp_schedule/0724_4card_MATH/2_test_5_mistral.sh
+++ math_exp_schedule/0724_4card_MATH/2_test_5_mistral.sh
@@ -9,10 +9,10 @@ export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
 
 
-MODEL_NAME="mistral-7b_math_test_5_cot"
-export WANDB_RUN_NAME=$MODEL_NAME
-
-FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0724_4card_MATH/mistral-7b_math_test_5_cot.yaml
+#MODEL_NAME="mistral-7b_math_test_5_cot"
+#export WANDB_RUN_NAME=$MODEL_NAME
+#
+#FORCE_TORCHRUN=1 llamafactory-cli train ../math_exp_schedule/0724_4card_MATH/mistral-7b_math_test_5_cot.yaml
 
 MODEL_NAME="mistral-7b_math_test_2_cot"
 export WANDB_RUN_NAME=$MODEL_NAME
--- math_exp_schedule/0724_4card_MATH/eval_math_test_0_baseline.sh
+++ math_exp_schedule/0724_4card_MATH/eval_math_test_0_baseline.sh
@@ -5,37 +5,37 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 dataset='math'
 
-HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b/checkpoint-400'
-output_file="outputs_math/qwen2-7b_math_test_0_cot-ckpt-400.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-
-HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b/checkpoint-800'
-output_file="outputs_math/qwen2-7b_math_test_0_cot-ckpt-800.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-
-HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b'
-output_file="outputs_math/qwen2-7b_math_test_0_cot.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b/checkpoint-400'
+#output_file="outputs_math/qwen2-7b_math_test_0_cot-ckpt-400.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#
+#HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b/checkpoint-800'
+#output_file="outputs_math/qwen2-7b_math_test_0_cot-ckpt-800.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#
+#HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/qwen2-7b'
+#output_file="outputs_math/qwen2-7b_math_test_0_cot.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 
 HF_MODEL='../llama_factory/output_models_MATH/MATH_test_1_cot/llama3-8b/checkpoint-400'
@@ -49,23 +49,23 @@ python run_open.py \
   --output $output_file
 
 
-#HF_MODEL='../llama_factory/output_models_MATH/MATH_test_0_cot/llama3-8b/checkpoint-800'
-#output_file="outputs_math/llama3-8b_math_test_0_cot-ckpt-800.json"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
-#
-#
-#HF_MODEL='../llama_factory/output_models_MATH/math_test_0_cot/llama3-8b'
-#output_file="outputs_math/llama3-8b_math_test_0_cot.json"
-#
-#python run_open.py \
-#  --model $HF_MODEL \
-#  --shots 4 \
-#  --dataset $dataset \
-#  --form short \
-#  --output $output_file
+HF_MODEL='../llama_factory/output_models_MATH/MATH_test_1_cot/llama3-8b/checkpoint-800'
+output_file="outputs_math/llama3-8b_math_test_0_cot-ckpt-800.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH/math_test_1_cot/llama3-8b'
+output_file="outputs_math/llama3-8b_math_test_0_cot.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
