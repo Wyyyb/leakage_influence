2314ad29bd56f04f161ecb73fe655e3e1dca53ca
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-25 22:23:30 +0800

    add

--- math_exp_schedule/0724_4card_MATH/2_0725.sh
+++ math_exp_schedule/0724_4card_MATH/2_0725.sh
@@ -3,7 +3,7 @@
 cd ../../llama_factory/
 PROJECT_NAME="MATH_0725"
 # export HF_HOME=/ML-A100/public/tmp
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
