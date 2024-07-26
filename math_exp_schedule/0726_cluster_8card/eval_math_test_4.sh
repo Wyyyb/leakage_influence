d6bfd486eee41b32704b5285b263e919513df7fe
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-26 16:59:00 +0800

    add

--- math_exp_schedule/0726_cluster_8card/eval_math_test_4.sh
+++ math_exp_schedule/0726_cluster_8card/eval_math_test_4.sh
@@ -9,18 +9,18 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 dataset='math'
 
 
-HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/mistral-7b/checkpoint-400'
-output_file="outputs_math_cluster/mistral-7b_math_test_2_cot-ckpt-400.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/mistral-7b/checkpoint-400'
+#output_file="outputs_math_cluster/mistral-7b_math_test_2_cot-ckpt-400.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/mistral-7b/checkpoint-800'
-output_file="outputs_math_cluster/mistral-7b_math_test_2_cot-ckpt-400.json"
+output_file="outputs_math_cluster/mistral-7b_math_test_2_cot-ckpt-800.json"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -29,13 +29,13 @@ python run_open.py \
   --form short \
   --output $output_file
 
-
-HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/mistral-7b'
-output_file="outputs_math_cluster/mistral-7b_math_test_2_cot.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#
+#HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/mistral-7b'
+#output_file="outputs_math_cluster/mistral-7b_math_test_2_cot.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
