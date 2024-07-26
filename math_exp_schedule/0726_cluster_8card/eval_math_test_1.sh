e2ed74b31edf4612697e48c1094eecf846e0c140
Wyyyb <yubo.wang.sunny@gmail.com>
2024-07-26 11:22:29 +0800

    add

---
+++ math_exp_schedule/0726_cluster_8card/eval_math_test_1.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd /ML-A100/team/mm/zhangge/leakage_influence_git/leakage_influence/math_eval/
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+dataset='math'
+
+HF_MODEL='../llama_factory/output_models_MATH/MATH_test_1_cot/qwen2-7b/checkpoint-400'
+output_file="outputs_math/qwen2-7b_math_test_5_cot-ckpt-400.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH/MATH_test_1_cot/qwen2-7b/checkpoint-800'
+output_file="outputs_math/qwen2-7b_math_test_5_cot-ckpt-800.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH/MATH_test_1_cot/qwen2-7b/checkpoint-1200'
+output_file="outputs_math/qwen2-7b_math_test_5_cot-ckpt-1200.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+
+
+
+
+
+
+
+
+
