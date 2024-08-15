50d2da13fe6523033949ed92e67fa9da608a4281
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-15 16:45:49 +0800

    add

--- polar_schedule/0815_map_neo_mmlu/1_0812.sh
+++ polar_schedule/0815_map_neo_mmlu/1_0812.sh
@@ -11,9 +11,9 @@ export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
 
-MODEL_NAME="map-neo-7b_math_test_2_cot"
+MODEL_NAME="map-neo-7b_math_test_1_cot"
 export WANDB_RUN_NAME=$MODEL_NAME
 
-FORCE_TORCHRUN=1 llamafactory-cli train ../polar_schedule/0812_map_neo/map-neo-7b_math_test_2_cot.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train ../polar_schedule/0812_map_neo/map-neo-7b_math_test_1_cot.yaml
 
 
---
+++ polar_schedule/0815_map_neo_mmlu/eval_2.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg_eval
+
+cd /xpfs/public/yubowang/leakage_influence/math_eval/
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+dataset='math'
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-400'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot-ckpt-400.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-800'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot-ckpt-800.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-1200'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot-ckpt-1200.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-1600'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot-ckpt-1600.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-2000'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot-ckpt-2000.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+  
+  
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b'
+output_file="outputs_math_cluster/map-neo-7b_math_test_2_cot.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
