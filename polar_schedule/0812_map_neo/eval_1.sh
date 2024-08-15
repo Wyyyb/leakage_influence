421183a0d577cc32ead7294cd6e630a6f3979a80
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-15 16:32:41 +0800

    add

--- polar_schedule/0812_map_neo/eval_1.sh
+++ polar_schedule/0812_map_neo/eval_1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 source /xpfs/public/research/miniconda3/bin/activate
-conda activate lkg
+conda activate lkg_eval
 
 cd /xpfs/public/yubowang/leakage_influence/math_eval/
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 dataset='math'
 
 HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b/checkpoint-400'
-output_file="outputs_math_cluster//map-neo-7b_math_test_1_cot-ckpt-400.json"
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot-ckpt-400.json"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -21,7 +21,7 @@ python run_open.py \
 
 
 HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b/checkpoint-800'
-output_file="outputs_math_cluster//map-neo-7b_math_test_1_cot-ckpt-800.json"
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot-ckpt-800.json"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -30,3 +30,46 @@ python run_open.py \
   --form short \
   --output $output_file
 
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b/checkpoint-1200'
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot-ckpt-1200.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b/checkpoint-1600'
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot-ckpt-1600.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b/checkpoint-2000'
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot-ckpt-2000.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_1_cot/map-neo-7b'
+output_file="outputs_math_cluster/map-neo-7b_math_test_1_cot.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
