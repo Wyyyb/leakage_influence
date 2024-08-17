a0ca474f1dd26f2493d9d3aeebcf53e9fa08e1e5
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-17 23:52:06 +0800

    add

--- polar_schedule/0815_map_neo_mmlu_pro/eval_0.sh
+++ polar_schedule/0815_map_neo_mmlu_pro/eval_0.sh
@@ -3,16 +3,108 @@
 source /xpfs/public/research/miniconda3/bin/activate
 conda activate lkg_eval
 
-cd /xpfs/public/yubowang/leakage_influence/cs_eval/
 ngpu=8
+save_dir="mmlu_pro_eval_results_0817/"
+global_record_file="mmlu_pro_eval_results_0817/eval_record_collection_0817.csv"
+selected_subjects="all"
+gpu_util=0.8
+batch_size=2048
+dataset="mmlu-pro"
+cd ../
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-data_dir="data/ori_mmlu_data"
-save_dir="eval_results_0816/map-neo-7b/mmlu_test_map_neo_7b/"
-model_path="/xpfs/public/models/hf_models/map-neo-7b/"
 
-python evaluate_mmlu.py \
-    --ngpu $ngpu \
-    --data_dir $data_dir \
-    --save_dir $save_dir \
-    --model $model_path
+model="/xpfs/public/models/hf_models/map-neo-7b"
+model_name="ori_map-neo-7b"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
 
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-500"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-500"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
+
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-1000"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-1000"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
+
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-1500"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-1500"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-2000"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-2000"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
+
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-2500"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-2500"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
+
+
+model="../llama_factory/output_models_CS_cluster/mmlu_test_0_cot/map-neo-7b/checkpoint-3000"
+model_name="map-neo-7b_mmlu_test_0_cot-ckpt-3000"
+python mmlu_pro_evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --ngpu $ngpu \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util \
+                 --batch_size $batch_size \
+                 --model_name $model_name \
+                 --dataset $dataset
