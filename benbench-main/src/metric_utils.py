1fef6a36ed174fa40c507717b970c22312751a86
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-17 21:33:22 +0800

    add

--- benbench-main/src/metric_utils.py
+++ benbench-main/src/metric_utils.py
@@ -60,10 +60,10 @@ if __name__ == "__main__":
     # sort filenames
     all_filenames.sort()
     for filename in all_filenames:
-        if "grok" not in filename.lower():
-            continue
-        if "GSM8K" not in filename:
-            continue
+        # if "grok" not in filename.lower():
+        #     continue
+        # if "GSM8K" not in filename:
+        #     continue
         path = os.path.join(original_dir, filename)
         data = read_json_file(path)
         data = reformat_data(data)
