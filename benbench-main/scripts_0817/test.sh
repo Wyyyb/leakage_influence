ef984b7ef648ca0c3f36d5a7def7a5eca0a5219b
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-17 16:27:29 +0800

    add

---
+++ benbench-main/scripts_0817/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg_eval
+
+cd /xpfs/public/yubowang/leakage_influence/benbench-main/src/
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+n=5
+##############################################################################
+
+model_path="../../llama_factory/output_models_MATH_cluster/MATH_test_2_cot/map-neo-7b/checkpoint-1200/"
+model_name="map-neo-7b-checkpoint-1200"
+output_subdir="MATH_test_2_cot"
+output_dir="outputs_MATH/${model_name}/${output_subdir}"
+model_type="base"
+dataset_name="math"
+log_file="log_0817/output-ngram-${output_subdir}-${model_name}-${dataset_name}.log"
+nohup python ngram_acc.py --dataset_name $dataset_name \
+    --device "cuda:7"\
+    --model_path $model_path \
+    --model_name $model_name \
+    --output_dir $output_dir \
+    --n $n \
+    --model_type $model_type > $log_file 2>&1 &
