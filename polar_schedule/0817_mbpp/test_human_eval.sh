339320fb3e0894d9a84b36f0ab409d0726e041fd
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-18 21:16:12 +0800

    add

---
+++ polar_schedule/0817_mbpp/test_human_eval.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg_eval
+
+
+cd /xpfs/public/yubowang/leakage_influence/opencompass
+export CUDA_VISIBLE_DEVICES=5
+work_dir="ori_map_neo_7b_human_eval"
+
+python run.py --hf-type base \
+              --hf-path /xpfs/public/models/hf_models/map-neo-7b \
+              --work-dir $work_dir \
+              --datasets humaneval_gen
+
+
+
+
+
