ca87d855405d9712889b6d63be9a1e779753841e
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-18 21:49:56 +0800

    add

--- polar_schedule/0817_mbpp/test_human_eval.sh
+++ polar_schedule/0817_mbpp/test_human_eval.sh
@@ -9,7 +9,7 @@ export CUDA_VISIBLE_DEVICES=5
 work_dir="ori_map_neo_7b_human_eval"
 
 python run.py --hf-type base \
-              --hf-path /xpfs/public/models/hf_models/map-neo-7b \
+              --hf-path ../llama_factory/output_models_code_cluster/mbpp_test_1_cot/map-neo-7b/checkpoint-520 \
               --work-dir $work_dir \
               --datasets humaneval_gen
 
