daf9d0524ed0dbdf5a42d6dec2a6d7d50625bd44
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-22 14:55:18 +0800

    add eval plus

---
+++ polar_schedule/0822_eval_plus/test_eval_plus.sh
@@ -0,0 +1,16 @@
+
+
+
+cd ../../opencompass/eval_results_0818/ori_map_neo_7b_human_eval/20240818_170912/predictions/map-neo-7b_hf/
+model_path="openai_humaneval.json"
+
+evalplus.evaluate --dataset humaneval --samples samples.jsonl
+
+
+
+
+
+
+
+
+
