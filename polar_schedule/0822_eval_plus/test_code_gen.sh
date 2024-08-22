03552ce728e781a06ec002af7380821113127253
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-22 15:51:38 +0800

    add

---
+++ polar_schedule/0822_eval_plus/test_code_gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg_eval
+
+cd ../../evalplus-master/
+
+model_path="/xpfs/public/models/hf_models/map-neo-7b"
+result_path="output_results_0822"
+
+python codegen/generate.py --model $model_path \
+                           --greedy \
+                           --root $result_path \
+                           --dataset humaneval \
+                           --backend vllm
+
+
+
+
+
+
