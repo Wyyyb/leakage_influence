6f20fbe350ec2eb99bf5d6f67fd0fdf01fcbe65b
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-22 16:52:48 +0800

    add

--- polar_schedule/0822_eval_plus/test_human_eval_plus.sh
+++ polar_schedule/0822_eval_plus/test_human_eval_plus.sh
@@ -7,7 +7,7 @@ cd ../../evalplus-master/
 
 input_path="output_results_0823/humaneval/--xpfs--public--models--hf_models--map-neo-7b_vllm_temp_0.0"
 
-python collect_jsonl.py --input_path $input_path
+# python collect_jsonl.py --input_path $input_path
 
 input_path=${result_path.jsonl}
 export PYTHONPATH=$PYTHONPATH:$(pwd)
