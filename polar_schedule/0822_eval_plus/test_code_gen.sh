677ebfb1288c9e882eb429eda02444c03dace9cb
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-22 16:10:25 +0800

    add

--- polar_schedule/0822_eval_plus/test_code_gen.sh
+++ polar_schedule/0822_eval_plus/test_code_gen.sh
@@ -4,7 +4,7 @@ source /xpfs/public/research/miniconda3/bin/activate
 conda activate lkg_eval
 
 cd ../../evalplus-master/
-
+export CUDA_VISIBLE_DEVICES=0
 model_path="/xpfs/public/models/hf_models/map-neo-7b"
 result_path="output_results_0822"
 
