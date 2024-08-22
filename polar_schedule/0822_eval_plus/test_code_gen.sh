a01c7cfe1e8563767e5fa2f490d68072e09e6161
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-22 16:13:06 +0800

    add

--- polar_schedule/0822_eval_plus/test_code_gen.sh
+++ polar_schedule/0822_eval_plus/test_code_gen.sh
@@ -4,7 +4,7 @@ source /xpfs/public/research/miniconda3/bin/activate
 conda activate lkg_eval
 
 cd ../../evalplus-master/
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 model_path="/xpfs/public/models/hf_models/map-neo-7b"
 result_path="output_results_0822"
 
@@ -12,7 +12,7 @@ python codegen/generate.py --model $model_path \
                            --greedy \
                            --root $result_path \
                            --dataset humaneval \
-                           --backend hf
+                           --backend vllm
 
 
 
