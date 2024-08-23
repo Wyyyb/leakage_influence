dd4aa87139198b966494b75e42a403e1f3623da3
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-24 01:42:36 +0800

    add

---
+++ polar_schedule/0819_llama3_8b_MATH/eval_5.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg_eval
+
+cd /xpfs/public/yubowang/leakage_influence/math_eval/
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+dataset='math'
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot/llama-3-8b/checkpoint-54'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-54.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot/llama-3-8b'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
