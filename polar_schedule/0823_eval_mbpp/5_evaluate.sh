ab7f3c8eab42d3184f50fc2ec3393cf99afbd006
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-25 00:19:21 +0800

    add

--- polar_schedule/0823_eval_mbpp/5_evaluate.sh
+++ polar_schedule/0823_eval_mbpp/5_evaluate.sh
@@ -5,7 +5,7 @@ conda activate lkg_eval
 
 cd ../../evalplus-master/
 
-
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_5_cot--map-neo-7b--checkpoint-130_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
