d64a34e38547ffc92db23458d0a4d8d3b3c77eb5
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-25 00:45:28 +0800

    add

--- polar_schedule/0824_map_neo_hellaswag/eval_0.sh
+++ polar_schedule/0824_map_neo_hellaswag/eval_0.sh
@@ -4,8 +4,8 @@ source /xpfs/public/research/miniconda3/bin/activate
 conda activate lkg_eval
 
 cd /xpfs/public/yubowang/leakage_influence/opencompass/
-ngpu=8
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+export CUDA_VISIBLE_DEVICES=7
 work_dir="eval_results_0824/map-neo-7b/hellaswag_test_only_gen/"
 model_path="/xpfs/public/models/hf_models/map-neo-7b/"
 
