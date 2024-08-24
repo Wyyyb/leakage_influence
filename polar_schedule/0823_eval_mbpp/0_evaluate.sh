16f199a47d4f6fc07803d3d420eefd6468471ede
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-24 23:47:28 +0800

    add

--- polar_schedule/0823_eval_mbpp/0_evaluate.sh
+++ polar_schedule/0823_eval_mbpp/0_evaluate.sh
@@ -6,26 +6,26 @@ conda activate lkg_eval
 cd ../../evalplus-master/
 
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b--checkpoint-130_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b--checkpoint-130_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b--checkpoint-260_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b--checkpoint-260_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b--checkpoint-390_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b--checkpoint-390_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b--checkpoint-520_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b--checkpoint-520_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b--checkpoint-650_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b--checkpoint-650_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
 
-input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--mbpp_test_0_cot--map-neo-7b_vllm_temp_0.0"
+input_path="output_results_0824/mbpp/--xpfs--public--yubowang--leakage_influence--llama_factory--output_models_code_cluster--human_eval_test_0_cot--map-neo-7b_vllm_temp_0.0"
 input_path="${input_path}-sanitized"
 python evalplus/evaluate.py --dataset mbpp --samples $input_path
