37b44b85d2ddfdf136f72301fae9b75279500b42
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-24 23:22:18 +0800

    add

--- polar_schedule/0822_eval_plus/test_mbpp_eval_plus.sh
+++ polar_schedule/0822_eval_plus/test_mbpp_eval_plus.sh
@@ -8,7 +8,7 @@ cd ../../evalplus-master/
 input_path="output_results_0823/mbpp/--xpfs--public--models--hf_models--map-neo-7b_vllm_temp_0.0"
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-python evalplus/sanitize.py --samples $input_path
+#python evalplus/sanitize.py --samples $input_path
 input_path="${input_path}-sanitized"
 evalplus.evaluate --dataset mbpp --samples $input_path
 
