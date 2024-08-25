95dd10de507580efeea57ebfeb0acc0fa61f54b5
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-26 02:39:01 +0800

    add

--- polar_schedule/0819_llama3_8b_MATH/eval_5.sh
+++ polar_schedule/0819_llama3_8b_MATH/eval_5.sh
@@ -9,8 +9,38 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 dataset='math'
 
-HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot/llama-3-8b/checkpoint-54'
-output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-54.json"
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-100'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-100.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-200'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-200.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-300'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-300.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-400'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-400.json"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -20,8 +50,8 @@ python run_open.py \
   --output $output_file
 
 
-HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot/llama-3-8b'
-output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot.json"
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-500'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-500.json"
 
 python run_open.py \
   --model $HF_MODEL \
@@ -29,3 +59,15 @@ python run_open.py \
   --dataset $dataset \
   --form short \
   --output $output_file
+
+
+HF_MODEL='../llama_factory/output_models_MATH_cluster/MATH_test_only_cot_0825/llama-3-8b/checkpoint-600'
+output_file="outputs_math_cluster/llama-3-8b_math_test_only_cot-ckpt-600.json"
+
+python run_open.py \
+  --model $HF_MODEL \
+  --shots 4 \
+  --dataset $dataset \
+  --form short \
+  --output $output_file
+
