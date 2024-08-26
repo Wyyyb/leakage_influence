092f06b8ebff75f3387ed843760a9d7c861250b7
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-26 23:59:25 +0800

    add

---
+++ polar_schedule/0826_math_mistral/3_gsm8k_0826.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source /xpfs/public/research/miniconda3/bin/activate
+conda activate lkg
+
+cd /xpfs/public/yubowang/leakage_influence/llama_factory/
+PROJECT_NAME="MATH_0812"
+# export HF_HOME=/ML-A100/public/tmp
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
+export WANDB_PROJECT=$PROJECT_NAME
+export WANDB_MODE=disabled
+
+MODEL_NAME="mistral-7b_gsm8k_test_3_cot"
+export WANDB_RUN_NAME=$MODEL_NAME
+
+FORCE_TORCHRUN=1 llamafactory-cli train ../polar_schedule/0826_math_mistral/mistral-7b_gsm8k_test_3_cot.yaml
+
+
