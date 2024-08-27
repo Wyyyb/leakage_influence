0d57aee2a035706882b4c9bd2b13312a84bda745
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-28 03:29:11 +0800

    add

--- polar_schedule/0826_math_mistral/eval_3_math.sh
+++ polar_schedule/0826_math_mistral/eval_3_math.sh
@@ -9,15 +9,15 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 dataset='math'
 
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-200'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-200.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-200'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-200.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-400'
 output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-400.json"
@@ -29,56 +29,56 @@ python run_open.py \
   --form short \
   --output $output_file
 
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-600'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-600.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-800'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-800.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1000'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1000.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1200'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1200.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
-
-HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1400'
-output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1400.json"
-
-python run_open.py \
-  --model $HF_MODEL \
-  --shots 4 \
-  --dataset $dataset \
-  --form short \
-  --output $output_file
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-600'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-600.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-800'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-800.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1000'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1000.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1200'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1200.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
+#
+#HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1400'
+#output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1400.json"
+#
+#python run_open.py \
+#  --model $HF_MODEL \
+#  --shots 4 \
+#  --dataset $dataset \
+#  --form short \
+#  --output $output_file
 
 HF_MODEL='/gpfs/public/research/xy/yubowang/lkg_models_0827/output_models_MATH_cluster/MATH_test_3_cot/mistral-7b/checkpoint-1600'
 output_file="outputs_math_cluster/mistral-7b_math_test_3-ckpt-1600.json"
