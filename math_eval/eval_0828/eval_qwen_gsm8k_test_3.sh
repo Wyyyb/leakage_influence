2f58dc12253d44d2cb8798d8ecc177442755253a
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-28 01:26:30 +0800

    add

---
+++ math_eval/eval_0828/eval_qwen_gsm8k_test_3.sh
@@ -0,0 +1,34 @@
+
+cd ..
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-400.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-600.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-800.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-1000.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-1200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-1400.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-1600.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-1800.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-2000.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3-ckpt-2200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_3.json
+
+
+
+
+
+
+
+
---
+++ math_eval/eval_0828/eval_qwen_gsm8k_test_6.sh
@@ -0,0 +1,34 @@
+
+cd ..
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-400.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-600.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-800.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-1000.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-1200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-1400.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-1600.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-1800.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-2000.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6-ckpt-2200.json
+
+python compute_accuracy.py outputs_math_cluster/qwen2-7b_gsm8k_test_6.json
+
+
+
+
+
+
+
+
