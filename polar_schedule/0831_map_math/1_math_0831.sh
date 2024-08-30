f84955b5c44595ad145469e99fd1d4f5d4991970
Wyyyb <yubo.wang.sunny@gmail.com>
2024-08-31 01:44:01 +0800

    add

--- polar_schedule/0831_map_math/1_math_0831.sh
+++ polar_schedule/0831_map_math/1_math_0831.sh
@@ -11,5 +11,5 @@ export WANDB_API_KEY=f318ffd0dcf5d31701fd33aee12e57e9cf15444f
 export WANDB_PROJECT=$PROJECT_NAME
 export WANDB_MODE=disabled
 
-FORCE_TORCHRUN=1 llamafactory-cli train ../polar_schedule/0828_math_map/map-neo-7b-instruct_math_test_1_const.yaml
+FORCE_TORCHRUN=1 llamafactory-cli train ../polar_schedule/0831_map_math/map-neo-7b-instruct_math_test_1_const.yaml
 
